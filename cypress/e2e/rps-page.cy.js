fbb2e914f4d265e13ba90d789abc3c456e597ecc
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-22 20:44:35 -0800

    Write 1st test for Cypress Test Suite
    I'm going to be writing my test suite first to set up expectations I
    have for the rock paper scissors overhaul.
    
    This first test will test a future feature that prevents you from
    starting any rock paper scissors game unless you pick a gamemode first.

--- cypress/e2e/rps-page.cy.js
+++ cypress/e2e/rps-page.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-  })
-})
+const playButton = "[data-cy='play-button']";
+const resultsOutcome = "[data-cy='results-outcome']";
+
+describe("Rock Paper Scissors page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("1. prevents you from playing the game before selecting a gamemode", () => {
+    cy.get(resultsOutcome)
+      .contains("Complete a RPS gamemode!")
+      .and("not.have.class", "accent");
+      
+    cy.get(playButton).click();
+
+    cy.get(resultsOutcome)
+      .contains("Please select a gamemode first!")
+      .and("have.class", "accent");
+  });
+});
