1055c2ca34f9cfd80d50c305802c35f964ec4705
Issac In <66139562+issac-in@users.noreply.github.com>
2022-12-22 22:58:12 -0800

    Write 3rd & 4th tests for Cypress Test Suite
    Technically, 4x tests since it's 3, 3A, 4, 4A, but I digress.
    The tests 3 & 4 are just to check that I can initialize the two
    respective gamemodes I plan to have for this rps overhaul. The tests 3A
    and 4A are to verify that I can play through each respective gamemode
    with no issues with the end outcome, even if the computer's choices are
    randomized.

--- cypress/e2e/rps-page.cy.js
+++ cypress/e2e/rps-page.cy.js
@@ -3,6 +3,60 @@ const bestOfMode = "[data-cy='best-of-mode']";
 const playButton = "[data-cy='play-button']";
 const resultsOutcome = "[data-cy='results-outcome']";
 
+function recursiveGame() {
+  cy.get("[data-cy='rock-button']").then(($rockButton) => {
+    if ($rockButton.is(":disabled")) { return; }
+    else { 
+      cy.wrap($rockButton).click();
+      recursiveGame();
+    }
+  });
+}
+
+function evaluateFirstTo() {
+  let score;
+  cy.get("[data-cy='player-score']").then(($playerScore) => {
+    score = $playerScore.text();
+    if (score === "5") {
+      cy.get(resultsOutcome)
+        .contains("You won! The computer lost to you.");
+    }
+  });
+  cy.get("[data-cy='computer-score']").then(($computerScore) => {
+    score = $computerScore.text();
+    if (score === "5") {
+      cy.get(resultsOutcome)
+        .contains("You lost! The computer beat you.");
+    }
+  });
+}
+
+function evaluateBestOf() {
+  cy.get("[data-cy='player-score']").then(($playerScore) => {
+    const pScore = parseInt($playerScore.text());
+
+    cy.get("[data-cy='computer-score']").then(($computerScore) => {
+      const cScore = parseInt($computerScore.text());
+
+      if (pScore > cScore) {
+        cy.get(resultsOutcome)
+          .contains("You won! The computer lost to you.");
+      }
+      else if (pScore === cScore) {
+        cy.get(resultsOutcome)
+          .contains("You tied! The computer tied you.");
+      }
+      else if (pScore < cScore) {
+        cy.get(resultsOutcome)
+          .contains("You lost! The computer beat you.");
+      }
+      else {
+        cy.fail("Unexpected outcome for evaluateBestO()");
+      }
+    });
+  });
+}
+
 describe("Rock Paper Scissors page", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -32,5 +86,49 @@ describe("Rock Paper Scissors page", () => {
       .click();
 
     cy.get(bestOfMode).should("be.checked");
+  });
+
+  it("3. lets you start the gamemode 'first to five' wins", () => {
+    cy.get(firstToMode).click();
+
+    cy.get(playButton).click();
+
+    cy.get(resultsOutcome)
+      .contains("Complete the first to five game.")
+      .and("not.have.class", "accent");
+  });
+
+  it("\u30003A. displays correct result at the end of a first to five",() => {
+    cy.get(firstToMode).click();
+
+    cy.get(playButton).click();
+
+    recursiveGame();
+
+    cy.get(resultsOutcome).should("have.class", "accent");
+
+    evaluateFirstTo();
+  });
+
+  it("4. lets you start the gamemode 'best of five' wins", () => {
+    cy.get(bestOfMode).click();
+
+    cy.get(playButton).click();
+
+    cy.get(resultsOutcome)
+      .contains("Complete the best of five game.")
+      .and("not.have.class", "accent");
+  });
+
+  it("\u30004A. displays correct result at the end of a best of five",() => {
+    cy.get(bestOfMode).click();
+
+    cy.get(playButton).click();
+
+    recursiveGame();
+
+    cy.get(resultsOutcome).should("have.class", "accent");
+
+    evaluateBestOf();
   });
 });
