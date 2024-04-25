dbae61a43a3ba2da2d29b11390655c2b56ed6c72
Issac In <66139562+issac-in@users.noreply.github.com>
2024-04-24 20:30:29 -0700

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+"use strict";
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let choice = "";
+    switch (randomNum) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+        default:
+            console.error(`Expected num to be 0, 1, or 2. Got ${randomNum}.`);
+            return;
+    }
+    return choice;
+}   
