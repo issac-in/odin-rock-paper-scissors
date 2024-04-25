c79ab3d6d1ce6e1840986f61193707af2f80ceee
Issac In <66139562+issac-in@users.noreply.github.com>
2024-04-24 20:59:33 -0700

    Write the logic to get the human choice

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 "use strict";
 
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
-    let choice = "";
+    let computerChoice = "";
     switch (randomNum) {
         case 0:
-            choice = "rock";
+            computerChoice = ROCK;
             break;
         case 1:
-            choice = "paper";
+            computerChoice = PAPER;
             break;
         case 2:
-            choice = "scissors";
+            computerChoice = SCISSORS;
             break;
         default:
             console.error(`Expected num to be 0, 1, or 2. Got ${randomNum}.`);
             return;
     }
-    return choice;
+    return computerChoice;
 }   
+
+function getHumanChoice() {
+    let humanChoice = "";
+    let keepGoing = true;
+    while (keepGoing) {
+        let humanInput = prompt("Enter a choice: rock, paper, or scissors.");
+        humanInput = humanInput ? humanInput.toLowerCase() : "cancel";
+        switch (humanInput) {
+            case ROCK:
+            case "r":
+                humanChoice = ROCK;
+                keepGoing = false;
+                break;
+            case PAPER:
+            case "p":
+                humanChoice = PAPER;
+                keepGoing = false;
+                break;
+            case SCISSORS:
+            case "s":
+                humanChoice = SCISSORS;
+                keepGoing = false;
+                break;
+            case "cancel":
+                console.warn("User cancelled their choice.");
+                keepGoing = false;
+                break;
+            default:
+                console.warn(`Choose between r, p, or s - not ${humanInput}.`);
+        }
+    }
+    return humanChoice;
+}
